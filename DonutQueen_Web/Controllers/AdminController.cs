3e2832a7eed1f307e582f4dace2a2b735c691fe2
yene <yene@DESKTOP-P3JV8C2>
2022-09-19 13:35:02 +0200

    Add project files.

---
+++ DonutQueen_Web/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DonutQueen_Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Donuts()
+        {
+            return View();
+        }
+        public IActionResult Gebruikers()
+        {
+            return View();
+        }
+        public IActionResult Orders()
+        {
+            return View();
+        }
+        public IActionResult Winkels()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ DonutQueen_Web/Controllers/WinkelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DonutQueen_Web.Controllers
+{
+    public class WinkelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
